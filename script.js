1284523035dc4c425feab343d784562e30ee638d
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-11-24 12:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const statusDiv = document.getElementById("status");
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+const disconnectButton = document.getElementById("disconnect-button");
+const notificationsDiv = document.getElementById("notifications");
+const wsUrl = "wss://socketsbay.com/wss/v2/1/demo/";
+let socket;
+
+const displayNotification = (message) => {};
+
+const connectWebSocket = () => {};
+
+sendButton.addEventListener("click", () => {});
+
+disconnectButton.addEventListener("click", () => {});
+
+connectWebSocket();
